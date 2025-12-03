267ef762fe850c780dd95c045e0344c6defff22c
AEslemCekici <250541026@firat.edu.tr>
2025-12-03 20:30:12 +0300

    Update Exercise4_6_Multadd.java

--- Exercise4_6_Multadd.java
+++ Exercise4_6_Multadd.java
@@ -1,38 +1,38 @@
 public class Multadd {
 
-    // a * b + c
+
     public static double multadd(double a, double b, double c) {
         return a * b + c;
     }
 
 
     public static double expSum(double x) {
-
         return multadd(x, Math.exp(-x), 1.0 - Math.exp(-x));
     }
 
     public static void main(String[] args) {
 
-        //  (1 * 2) + 3
-        System.out.println(multadd(1.0, 2.0, 3.0));
 
-        //  cos * 0.5 + sin
-        System.out.println(multadd(Math.cos(Math.PI / 4), 0.5, Math.sin(Math.PI / 4)));
+        System.out.println(" multadd(1, 2, 3) sonucu: " + multadd(1.0, 2.0, 3.0));
 
-        //  1.0 * log10 + log20
-        System.out.println(multadd(1.0, Math.log(10), Math.log(20)));
+      // Trigonometri
+        System.out.println(" sin(pi/4) + cos(pi/4)/2 sonucu: " +
+                multadd(Math.cos(Math.PI / 4), 0.5, Math.sin(Math.PI / 4)));
 
-        //  expSum fonksiyonu (x = 2.0 için)
-        System.out.println(expSum(2.0));
+       // Logaritma
+        System.out.println(" log(10) + log(20) sonucu: " +
+                multadd(1.0, Math.log(10), Math.log(20)));
+
+
+        System.out.println(" expSum(2.0) sonucu: " + expSum(2.0));
     }
 }
 
-
 /*
-Çıktı:
-5.0
-1.0606601717798212
-5.298317366548037
-1.1353352832366128
+multadd(1, 2, 3) sonucu: 5.0
+sin(pi/4) + cos(pi/4)/2 sonucu: 1.0606601717798212
+log(10) + log(20) sonucu: 5.298317366548037
+expSum(2.0) sonucu: 1.1353352832366128
+
 */
 
