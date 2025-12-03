75073ef4df49adb63c3a3400f8fd6074dded68dd
AEslemCekici <250541026@firat.edu.tr>
2025-12-03 20:19:43 +0300

    Update Exercise4_6_Multadd.java

--- Exercise4_6_Multadd.java
+++ Exercise4_6_Multadd.java
@@ -1,16 +1,28 @@
-public class Exercise4_6_Multadd {
+public class Multadd {
 
+    // a * b + c
     public static double multadd(double a, double b, double c) {
-        // TODO
-        return 0.0;
+        return a * b + c;
     }
 
+
     public static double expSum(double x) {
-        // TODO
-        return 0.0;
+
+        return multadd(x, Math.exp(-x), 1.0 - Math.exp(-x));
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+
+        //  (1 * 2) + 3
+        System.out.println(multadd(1.0, 2.0, 3.0));
+
+        //  cos * 0.5 + sin
+        System.out.println(multadd(Math.cos(Math.PI / 4), 0.5, Math.sin(Math.PI / 4)));
+
+        //  1.0 * log10 + log20
+        System.out.println(multadd(1.0, Math.log(10), Math.log(20)));
+
+        //  expSum fonksiyonu (x = 2.0 için)
+        System.out.println(expSum(2.0));
     }
 }
